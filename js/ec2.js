f02642fcfcb966494a15b269e53d505bd9d2b64d
Catalin Chinchisan <40236500+catalinchinchisan@users.noreply.github.com>
2018-10-12 18:47:13 +0300

    solve

--- js/ec2.js
+++ js/ec2.js
@@ -5,7 +5,7 @@ function solve(){
     var a = document.getElementById("id_a").value;
     var b = document.getElementById("id_b").value;
     var c = document.getElementById("id_c").value;
-    var delta = b*b-4*ac;
+    var delta = b*b-4*a*c;
     var x1_re,x1_im,x2_re,x2_im;
     if(delta < 0){
         x1_re =  -b / (2 * a);
