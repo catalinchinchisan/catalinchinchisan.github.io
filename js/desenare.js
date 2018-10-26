5eac2ed2f59481cf8676c9713d9d536bf6e59ef8
Catalin Chinchisan <40236500+catalinchinchisan@users.noreply.github.com>
2018-10-26 19:27:59 +0300

    new commit

--- js/desenare.js
+++ js/desenare.js
@@ -1,10 +1,46 @@
-document.getElementById("id_business_version").innerHTML = "Business version 2018.10.26.0"
+document.getElementById("id_business_version").innerHTML = "Business version 2018.10.26.4"
 document.getElementById("id_start").addEventListener("click", start);
+document.getElementById("id_stop").addEventListener("click", stop);
+
+document.getElementById("id_start").disabled = false;
+document.getElementById("id_stop").disabled = true;
+
+document.getElementById("id_stop").addEventListener("click", stop);
+
+var alpha = {unghi:0};
+function deseneazaCerc(context, w, h,alpha){
+    
+    alpha.unghi++;
+    context.clearRect(0, 0, w, h);
+
+    var r = 100;
+    
+    var x = w / 2 + r * Math.cos(alpha.unghi * Math.PI  / 180);
+    var y = h / 2 + r * Math.sin(alpha.unghi * Math.PI  / 180);
+        
+    context.beginPath();
+    context.arc(x, y, 50, 0, 2 * Math.PI);
+    context.stroke();
+    
+}
+
 function start(){
+    document.getElementById("id_start").disabled = true;
+    document.getElementById("id_stop").disabled = false;
     var canvas = document.getElementById("id_canvas");
     var context = canvas.getContext("2d");
     
-    context.beginPath();
-    context.arc(canvas.width / 2, canvas.height / 2, 50, 0, 2 * Math.PI);
-    context.stroke();
+    intervalId = setInterval(  deseneazaCerc, 10, context, canvas.width, canvas.height, alpha, 100);
+
+    var my_worker = new Worker("/calcul_prime.js");
+    my_worker.onmessage = function(e){
+        document.getElementById("id_prime").innerHTML = e.data;
+    }
+    
+}
+
+function stop(){
+    clearInterval(intervalId);
+    document.getElementById("id_start").disabled = false;
+    document.getElementById("id_stop").disabled = true;
 }
