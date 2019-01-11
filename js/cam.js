37c6cb556e70581a8911f764521b9f70f63ab536
unknown <catalin10_chinchisan@yahoo.com>
2019-01-11 18:15:03 +0200

    cam

---
+++ js/cam.js
@@ -0,0 +1,13 @@
+document.getElementById("id_logic_version").innerHTML = "Logic version=2019.01.11.0";
+document.getElementById("id_button").addEventListener("click", on_get_cam());
+
+function on_cam_error(){
+    document.getElementById("id_video").srcobject = e;
+}
+function on_cam_ok(e){
+    alert("cam error")
+}
+function on_get_cam(){
+    var c = {audio: true, video:true};
+    navigator.mediaDevices.getUserMedia(c, on_cam_ok, on_cam_error)
+}
